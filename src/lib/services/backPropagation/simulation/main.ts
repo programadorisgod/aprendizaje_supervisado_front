8e38eefba72dc47020c261fa0f1c82dbbf80b98b
Jesus Bermudez <jesmanuelber@gmail.com>
2024-05-01 10:14:52 -0500

    ADD: Simulation module of backpropagation

--- src/lib/services/backPropagation/simulation/main.ts
+++ src/lib/services/backPropagation/simulation/main.ts
@@ -1,4 +1,7 @@
 import fetchData from '$lib/utils/fetchData'
+import { sin } from '../activationFunctions/seno'
+import { sigmoid } from '../activationFunctions/sigmoid'
+import { tanh } from '../activationFunctions/tanh'
 import type { layerManagement, networkLayer } from '../interface'
 
 export default async function simulation(
@@ -33,11 +36,36 @@ export default async function simulation(
 		const h: number[][] = [dataBase[p][0], ...layers.map((v) => new Array(v.neurons).fill(0))]
 
 		for (let c = 0; c < layers.length; c++) {
-			for (let i = 0; i < array.length; i++) {
-				const element = array[i]
+			for (let i = 0; i < aux.indexes[aux.current + 1]; i++) {
+				for (let j = 0; j < aux.indexes[aux.current]; j++) {
+					h[c + 1][i] += h[c][j] * w[c][i][j]
+				}
+
+				// Restar el umbral
+				h[c + 1][i] -= u[c][i]
+
+				// Funciones de activacion
+				switch (layers[c].activationFunction) {
+					case 'TH':
+						// h sub i es igual a fa(suma - u)
+						h[c + 1][i] = tanh(h[c + 1][i])
+
+						break
+					case 'SG':
+						h[c + 1][i] = sigmoid(h[c + 1][i])
+
+						break
+					case 'SENO':
+						h[c + 1][i] = sin(h[c + 1][i])
+
+						break
+				}
 			}
+			aux.current++
 		}
+		yr.push(h.at(-1)!)
+		aux.current = 0
 	}
 
-	return
+	return yr
 }
